4d904254deb9f3fd16a61e348cfaf59158896f48
mehedi hasan <mhshohan04@gmail.com>
2025-09-06 23:51:19 +0600

    render news with category

--- script.js
+++ script.js
@@ -29,8 +29,35 @@ function displayCategory(categories) {
     // set indicator on clicked item
     if(e.target.localName === 'li'){
       e.target.classList.add('border-b-4');
+      loadNewsByCategory(e.target.id);
+      // console.log(e.target.id);
     }
   })
 }
+
+const loadNewsByCategory = (category_id) => {
+  const url = `https://news-api-fs.vercel.app/api/categories/${category_id}`;
+  fetch(url)
+  .then(res => res.json())
+  .then(data => showNewsByCategory(data))
+  .catch(err => console.error("Error fetching data:", err));
+}
+
+const showNewsByCategory = (news) => {
+  console.log(news.articles);
+  const newsContainer = document.getElementById('news-container');
+  newsContainer.innerHTML = "";
+
+  news.articles.forEach(article => {
+    newsContainer.innerHTML += `
+      <div class="border p-4 rounded-lg shadow-lg">
+        <img src="${article.image.srcset[5].url}" alt="${article.title}" class="w-full h-48 object-cover rounded-md mb-4">
+        <h2 class="text-xl font-bold mb-2">${article.title}</h2>
+      </div>
+
+`})
+
+}
+
 
 renderCategory();
